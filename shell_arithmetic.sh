d3111ffe79cc1668960b77845d2b5bc0dab0023e
Madhu Chakravarthy <ncmadhu@gmail.com>
2017-04-05 18:12:21 +0530

    Shell arithmetic examples

---
+++ shell_arithmetic.sh
@@ -0,0 +1,9 @@
+#############################
+# shell arithmetic examples #
+#############################
+
+x= expr 1 + 3
+echo -n $x
+echo "expr 1 + 3"; # will not work
+echo 'expr 1 + 3'; # will not work
+echo `expr 1 + 3`; # will work
