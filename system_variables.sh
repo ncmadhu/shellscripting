72c2e916d19c856fbacd6b41f6cccf1a1d8ef580
Madhu Chakravarthy <ncmadhu@gmail.com>
2017-04-05 15:52:15 +0530

    System variables

---
+++ system_variables.sh
@@ -0,0 +1,12 @@
+###################################################
+# system variables are defined in capital letters #
+###################################################
+
+echo $BASH
+echo $BASH_VERSION
+echo $HOME
+echo $LOGNAME
+echo $PATH
+echo $OSTYPE
+echo $PWD
+echo $SHELL
