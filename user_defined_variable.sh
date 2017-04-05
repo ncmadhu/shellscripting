eb4d7db3148bb4a00c5c7ed07472ac7237876df7
Madhu Chakravarthy <ncmadhu@gmail.com>
2017-04-05 16:58:06 +0530

    User defined variables

---
+++ user_defined_variable.sh
@@ -0,0 +1,22 @@
+##################################
+# user defined variable examples #
+##################################
+
+# variable name should beging with alpha numeric character
+# no spaces on either side of equal sign
+# case sensitive
+
+test="Testing user defined variable"
+
+echo $test
+echo $Test;  # should not print anything
+
+Test="Change value"
+
+echo $test
+echo $Test
+
+# Null value
+
+null_value=
+echo $null_value
